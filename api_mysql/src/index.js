5d13a67ed0e322fae19e153269b66dda8bd29314
PHAMHAIDANG09 <dang2mui@gmail.com>
2024-11-01 12:03:40 +0700

    todoupdate

--- api_mysql/src/index.js
+++ api_mysql/src/index.js
@@ -1,6 +1,10 @@
 const app = require('./app');
-
+const express = require('express');////
+const bodyParser = require('body-parser');////
+const todoRoutes = require('./routes/todoRoutes');////
 const PORT = process.env.PORT || 3000;
+app.use(bodyParser.json()); //// Giúp parse dữ liệu JSON//
+app.use('/api', todoRoutes);////
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
